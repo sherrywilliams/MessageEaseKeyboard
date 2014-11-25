0d0e74c4ecb5d968e0cfbbb5cc66790636c8235f
Khoa Pham <onmyway133@gmail.com>
2014-11-25 11:14:15 +0700

    Initial commit

---
+++ MessageEaseKeyboard/KeyboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  KeyboardViewController.h
+//  MessageEaseKeyboard
+//
+//  Created by Khoa Pham on 11/25/14.
+//  Copyright (c) 2014 Fantageek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KeyboardViewController : UIInputViewController
+
+@end
---
+++ README.md
@@ -0,0 +1,8 @@
+MessageEase
+=========
+Learn how to make custom keyboard extension
+
+
+License
+--
+This project is released under the MIT license. See LICENSE.md
